117fca1fac56d575feeed3823b54f7bccfa23826
pvineet44 <pvineet44@gmail.com>
2020-07-28 16:42:06 +0200

    #2

--- scripts.js
+++ scripts.js
@@ -15,17 +15,66 @@ var Blogs = Backbone.Collection.extend({});
 
 //Instantiate two blogs
 
-var blog1 = new Blog({
-    author: 'Narendra',
-    title: 'Modi\'s blog',
-    url : 'http://namo.in'
+// var blog1 = new Blog({
+//     author: 'Narendra',
+//     title: 'Modi\'s blog',
+//     url : 'http://namo.in'
+// });
+
+
+// var blog2 = new Blog({
+//     author: 'Amit',
+//     title: 'Mota bhai\'s blog',
+//     url : 'http://bjp.in'
+// });
+
+var blogs = new Blogs();
+
+//Backbone view for one blog
+
+var BlogView = Backbone.View.extend({
+    model: new Blog(),
+    tagName: 'tr',
+    initialize: function() {
+        this.template = _.template($('.blogs-list-template').html())
+    },
+    render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
 });
 
 
-var blog2 = new Blog({
-    author: 'Amit',
-    title: 'Mota bhai\'s blog',
-    url : 'http://bjp.in'
+//Backbone view for all blogs
+var BlogsView = Backbone.View.extend({
+    model: blogs,
+    el: $('.blogs-list'),
+    initialize: function() {
+        this.model.on('add', this.render, this);
+    },
+    render: function() {
+        var self = this;
+        this.$el.html('');
+        _.each(this.model.toArray(), function(blog) {
+            self.$el.append((new BlogView({model: blog})).render().$el);
+        });
+        return this;
+    }
 });
 
-var blogs = new Blogs([blog1, blog2]);
+var blogsView = new BlogsView();
+
+$(document).ready(function (){
+    $('.add-blog').on('click', function(){
+        var blog = new Blog({
+            author: $('.author-input').val(),
+            title: $('.title-input').val(),
+            url: $('.url-input').val(),
+        });
+        $('.author-input').val('');
+        $('.title-input').val('');
+        $('.url-input').val('');
+        console.log(blog.toJSON());
+        blogs.add(blog);
+    })
+})
